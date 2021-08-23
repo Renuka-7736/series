07835e458e4037686e9ee53bd0c2459af1fb5267
Renuka-7736 <89129999+Renuka-7736@users.noreply.github.com>
2021-08-23 13:05:50 +0530

    Create series.c
    series

---
+++ series.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i ,j,n;
+printf("enter the value of n");
+scanf("%d",&n);
+for(int i=o;i<n;i++)
+{
+for(int j=o;j<i;j++);{
+  printf("%d",(i+j+1)%2);
+  }
+  printf("\n");
+  }
+  }
+  
